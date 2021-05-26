fe88144b984cfe74f7326a66a71c0e53f8883ce3
Enrrique Pimentel <65453362+enrriquep3@users.noreply.github.com>
2021-05-26 12:20:13 -0400

    Add files via upload

---
+++ guess.py
@@ -0,0 +1,13 @@
+name = input("Enter your name: ")
+print("Hello", name + "!")
+age = int(input("What year did you born: "))
+print("You are", 2021 - age, "Years old")
+fav_sport = input("What is your Favorite Sport?: ")
+print("Your Favorite Sport is:", fav_sport + "!")
+fav_food = input("What is your Favorite Food?: ")
+print("Your Favorite Food is:", fav_food + "!")
+print("I know A lot about you, Your name is:", name, "You are", 2021 - age, "Years old, " "Your Favorite Sport is", fav_sport + ", " "Your Favorite Food is", fav_food + ".")
+
+
+
+
