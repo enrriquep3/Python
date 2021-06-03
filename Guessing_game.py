15bfb12d05654e4a764d898364e01f294349963c
Enrrique Pimentel <65453362+enrriquep3@users.noreply.github.com>
2021-06-03 08:40:21 -0400

    Add files via upload

--- Guessing_game.py
+++ Guessing_game.py
@@ -1,18 +1,18 @@
 # This program create a random name and ask the user to guess the number.
 
 import random
-num = random.randint(0, 9)
+num = random.randint(0, 101)
 run = True
 print('Welcome to the Guess Number Game.')
 while run:
-    guess = int(input("Enter your number between 0-9: "))
+    guess = int(input("Enter your number between 0-100: "))
 
     if guess == num:
         print("You Are Correct!")
         run = False
 
     if guess > num:
-        print("Your number is to high")
+        print("Your number is to high, keep trying")
 
     if guess < num:
-        print("Your number is too low")
+        print("Your number is too low, keep trying")
---
+++ week4Assi1.py
@@ -0,0 +1,19 @@
+# Program that counts down from 10 With while loop.
+number = 10
+
+while number > -1:
+    print('CountDown with While Loops = ', number)
+    number = number - 1
+
+# Program that counts down from 10 With for loop.
+for i in range(10, 0, -1):
+    print('CountDown with For Loops = ', i)
+
+# Write a program that prints 2n where n goes from 1 to 100
+for n in range(1, 101):
+    print('The number is:', 2**n)
+
+
+
+
+
